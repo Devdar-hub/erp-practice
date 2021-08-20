eec63b063dbd63622a7a7da80f49c61ef3d560c9
Pratik <guptapratik12121996@gmail.com>
2021-08-20 23:14:10 +0530

    lead list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 // import logo from './logo.svg';
-import './App.css';
-import Login from './Components/Login';
+import "./App.css";
+import Login from "./Components/Login";
 
-import { Admin } from './Components/Admin'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import React from 'react'
+import Admin  from "./Components/Admin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
 
 function App() {
-
   return (
     <>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route exact path="/Admin">
-          <Admin />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
           </Route>
-      </Switch >
-    </Router>
+          <Route exact path="/Admin">
+            <Admin />
+          </Route>
+        </Switch>
+      </Router>
     </>
-      );
+  );
 }
 
-      export default App;
+export default App;
--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,59 +1,133 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-export const Admin = () => {
-    function leadlist(){
-        var gettoken = localStorage.getItem('token')
-        
-        fetch("https://projects.parthvi.tech/api/v1/lead",{
-            method : 'GET',
-            headers : ({'Content-Type':'application/json',
-                        'Authorization':'Token '+gettoken})
-        }).then(response => response.json())
-            .then(data => {console.log(data);
-                if(data.status_code === 200)
-            {
-              
-                document.getElementById("errordiv").innerHTML="data received";            
-                
-                
-            }
-        else
-    {
-        document.getElementById("errordiv").innerHTML="Error from server";
-    }})
+function Admin() {
+  const [leads_list, setLeadsList] = useState(null);
+  const [contact_list, setContactList] = useState(null);
+
+  useEffect(() => {
+    getLeadlist();
+    async function getLeadlist() {
+      var gettoken = localStorage.getItem("token");
+
+      const response = await fetch(
+        "https://projects.parthvi.tech/api/v1/lead",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Token " + gettoken,
+          },
+        }
+      );
+      const data = await response.json();
+      if (data.status_code === 200) {
+        document.getElementById("errordiv").innerHTML = "data received";
+      } else {
+        document.getElementById("errordiv").innerHTML = "Error from server";
+      }
+      console.log(25, data);
+      setLeadsList(data.data);
     }
-    return (
-        <div>
-            <h1>lead List</h1>
-            <table style={{border:"2px solid black"}} id="leadtable">
-                <tr>
-                    <th>ID</th>
-                    <th>First name</th>
-                    <th>Last name</th>
-                    <th>Lead source</th>
-                    <th>Lead stage</th>
-                    <th>Phone No.</th>
-                </tr>
-                <tr>
-                    <td id="id"></td>
-                    <td id="fname"></td>
-                    <td id="lname"></td>
-                    <td id="lsource"></td>
-                    <td id="lstage"></td>
-                    <td id="ph"></td>
-                </tr>
-            </table>
-            {/* {data.map()} */
-            }
-            {/* <div>{data}</div> */}
-            <div id="errordiv"></div>
-            {leadlist()}
-        </div>
-    )
+
+    getContactlist();
+    async function getContactlist() {
+      var gettoken = localStorage.getItem("token");
+
+      const response = await fetch(
+        "https://projects.parthvi.tech/api/v1/contact/",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Token " + gettoken,
+          },
+        }
+      );
+      const data = await response.json();
+      if (data.status_code === 200) {
+        document.getElementById("errordiv").innerHTML = "data received";
+      } else {
+        document.getElementById("errordiv").innerHTML = "Error from server";
+      }
+      console.log(74, data);
+      setContactList(data.data);
+    }
+  }, []);
+
+  return (
+    <div>
+      <div id="id1">
+        <h1>lead List</h1>
+        <table style={{ border: "2px solid black" }} id="leadtable">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Lead source</th>
+              <th>Lead stage</th>
+              <th>Phone No.</th>
+            </tr>
+          </thead>
+          <tbody>
+            {leads_list &&
+              leads_list.map(function (item, index) {
+                return (
+                  <tr key={index}>
+                    <td>{item.first_name}</td>
+                  </tr>
+                );
+              })}
+            <tr>
+              <td id="id"></td>
+              <td id="fname"></td>
+              <td id="lname"></td>
+              <td id="lsource"></td>
+              <td id="lstage"></td>
+              <td id="ph"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div id="id2">
+        <h1>Contact List</h1>
+        <table style={{ border: "2px solid black" }} id="contacttable">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Lead source</th>
+              <th>Lead stage</th>
+              <th>Phone No.</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contact_list &&
+              contact_list.map(function (item, index) {
+                return (
+                  <tr key={index}>
+                    <td>{item.first_name}</td>
+                  </tr>
+                );
+              })}
+            <tr>
+              <td id="id"></td>
+              <td id="fname"></td>
+              <td id="lname"></td>
+              <td id="lsource"></td>
+              <td id="lstage"></td>
+              <td id="ph"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div id="errordiv"></div>
+    </div>
+  );
 }
 
-
-
-
-
-
+export default Admin;
